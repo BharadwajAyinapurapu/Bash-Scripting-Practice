69362ae3e556084cccff7acaf4fec93a073df2e6
BharaDwaj <bdisforall@gmail.com>
2022-01-20 23:21:29 +0530

    Added test script 1

---
+++ test1.sh
@@ -0,0 +1,7 @@
+bash user-input.sh ROLL
+bash user-input.sh ODDEVEN 43
+bash user-input.sh ODDEVEN 76
+bash user-input.sh PRIME 54
+bash user-input.sh PRIME 11
+bash user-input.sh SQUARE 12
+bash user-input.sh QUIT
