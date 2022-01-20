24d03bf2342111282202d4f01b8715df906323a9
BharaDwaj <bdisforall@gmail.com>
2022-01-20 23:56:47 +0530

    Modified test scripts - 1

--- test1.sh
+++ test1.sh
@@ -4,4 +4,6 @@ bash user-input.sh ODDEVEN 76
 bash user-input.sh PRIME 54
 bash user-input.sh PRIME 11
 bash user-input.sh SQUARE 12
+bash user-input.sh ROLL
+bash user-input.sh PRIME 324
 bash user-input.sh QUIT
