b4b9b4b534b398322fa4ef1c7e6abdc14bd38388
BHARADWAJ AYINAPURAPU <51354461+BharadwajAyinapurapu@users.noreply.github.com>
2022-01-23 11:37:21 +0530

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -1,7 +1,7 @@
 bash user-input.sh ROLL
 bash user-input.sh ODDEVEN 45
 bash user-input.sh ODDEVEN 74
-bash user-input.sh PRIME 52
+bash user-input.sh PRIME 54
 bash user-input.sh PRIME 11
 bash user-input.sh SQUARE 12
 bash user-input.sh ROLL
